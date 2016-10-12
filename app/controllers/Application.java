6ccc62ea8c21e99932326afebb740208c6966728
Jarquison <jacksonlira.rs@gmail.com>
2016-10-12 17:27:22 -0300

    Implementamos o método sayHello no controller.Aplication.java; em views alteramos o index aficioando um form e criamos a página sayHello que mostra a resposta.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,27 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
 
-import java.util.*;
-
+import play.mvc.Controller;
+import play.mvc.results.Result;
 import models.*;
+import play.data.*;
+import play.data.validation.Required;
+import views.*;
 
 public class Application extends Controller {
 
     public static void index() {
         render();
     }
+    
+    public static void sayHello(@Required String myName) {
+        if(validation.hasErrors()) {
+            flash.error("Oops, por favor entre com seu nome!");
+            index();
+        }
+        render(myName);
+    }
+    
+    
 
 }
